806064a20c35b3894eea1dd3498c05c70cd012c6
leand <leand@DESKTOP-NAKAMUR>
2022-02-13 21:53:08 -0300

    Add project files.

---
+++ TarefasApi/Endpoints/TarefasEndpoints.cs
@@ -0,0 +1,63 @@
+﻿using Dapper.Contrib.Extensions;
+using TarefasApi.Data;
+using static TarefasApi.Data.TarefaContext;
+
+namespace TarefasApi.Endpoints
+{
+    public static class TarefasEndpoints
+    {
+        public static void MapTarefasEndpoints(this WebApplication app)
+        {
+            app.MapGet("/", () => $"Bem-Vindo a API Tarefas {DateTime.Now}");
+
+            app.MapGet("/tarefas", async (GetConnection connectionGetter) =>
+            {
+                using var con = await connectionGetter();
+                var tarefas = con.GetAll<Tarefa>().ToList();
+
+                if (tarefas is null)
+                    return Results.NotFound();
+
+                return Results.Ok(tarefas);
+
+            });
+
+
+            app.MapGet("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+
+                return con.Get<Tarefa>(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound();
+            });
+
+
+            app.MapPost("/tarefas", async (GetConnection connectionGetter, Tarefa tarefa) => 
+            {
+                using var con = await connectionGetter();
+                var id = con.Insert(tarefa);
+                return Results.Created($"/tarefas/{id}", tarefa);
+            });
+
+
+            app.MapPut("/tarefas", async (GetConnection connectionGetter, Tarefa tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Update(tarefa);
+                return Results.Ok();
+            });
+
+
+            app.MapDelete("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+            {   
+                using var con = await connectionGetter();
+                var deleted = con.Get<Tarefa>(id);
+
+                if (deleted is null)
+                    return Results.NotFound();
+
+                con.Delete(deleted);
+                return Results.Ok(deleted);
+            });
+        }
+    }
+}
---
+++ TarefasApi/Extensions/sqlConnection.cs
@@ -0,0 +1,12 @@
+﻿namespace TarefasApi.Extensions
+{
+    internal class sqlConnection
+    {
+        private string connectionString;
+
+        public sqlConnection(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+    }
+}
